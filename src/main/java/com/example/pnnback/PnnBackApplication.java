966b8c25c5944aa523d76ece0d2c29806512576a
Attila-03 <opaewang8474@naver.com>
2025-05-13 19:48:21 +0900

    INIT

---
+++ src/main/java/com/example/pnnback/PnnBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.pnnback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PnnBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PnnBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pnnback/PnnBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pnnback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PnnBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
