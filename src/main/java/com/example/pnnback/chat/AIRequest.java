0545182f33f1261c6aab44bf2b348ff93fb802c1
Attila-03 <opaewang8474@naver.com>
2025-06-05 19:16:44 +0900

    GPT API

---
+++ src/main/java/com/example/pnnback/chat/AIRequest.java
@@ -0,0 +1,12 @@
+package com.example.pnnback.chat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AIRequest {
+    private String message;
+}
